ace3e3ab89d61808ea9be46abb3af5e809c81667
IgnaDev <arcortoon@gmail.com>
2023-12-01 21:22:00 -0300

    State complejo ejercicio

--- Part1/Apuntes/src/App.jsx
+++ Part1/Apuntes/src/App.jsx
@@ -1,12 +1,49 @@
+import { useState } from 'react'
 import './App.css'
-import Mensaje from './components/Mensaje'
 
 
 
 function App() {
+  // const [left, setLeft] = useState(0)
+  // const [right, setRight] = useState(0)
+  const [counters, setCounters] = useState({
+    left: 0,
+    right: 0
+  })
+
+
+  const handlerClick = (lado) =>{
+    if(lado === "left"){
+      setCounters({
+        ...counters,
+        left: counters.left + 1
+      })
+    }else{
+      setCounters({
+        ...counters,
+        right: counters.right + 1
+      })
+    }
+  }
+
   return (
     <>
-      <Mensaje mensaje={"Aqui desde app"}/>
+    <h1>Estado complejo | React y useState</h1>
+
+      {counters.left}
+      <button
+        onClick={()=>{handlerClick("left")}}
+      >
+        left
+      </button>
+
+      <button
+        onClick={()=>{handlerClick("right")}}
+      >
+        right
+      </button>
+      {counters.right}
+
     </>
   )
 }
