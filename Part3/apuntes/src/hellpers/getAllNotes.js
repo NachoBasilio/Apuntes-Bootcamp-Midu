8aee80d6945987392353aa0d05b594c30482ac76
IgnaDev <arcortoon@gmail.com>
2023-12-08 13:24:53 -0300

    Acomodando carpetas

--- Part3/apuntes/src/hellpers/getAllNotes.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default function getAllNotes (){
-    return axios.get("http://localhost:3001/api/notes")
-    .then(response =>{
-        const {data} = response
-        return data
-    })
-}   
