320dd8dd371648d38de9f1158213c54cd64c1608
IgnaDev <arcortoon@gmail.com>
2023-12-09 21:45:03 -0300

    Conectada la base de dato

---
+++ Part3/backend/mongo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const connectionString = 'mongodb+srv://admin:admin@notasmidu.ry7llh7.mongodb.net/notes?retryWrites=true&w=majority'
+
+// coneccion a mongoDB
+
+mongoose.connect(connectionString)
+  .then(() => {
+    console.log('Database connected')
+  }).catch(err => {
+    console.error(err)
+  })
