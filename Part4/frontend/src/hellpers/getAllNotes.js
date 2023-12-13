2ad427f06b8f2125298b73221237b1b1e2769316
IgnaDev <arcortoon@gmail.com>
2023-12-13 09:33:53 -0300

    Aplicando testing

---
+++ Part4/frontend/src/hellpers/getAllNotes.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default function getAllNotes (){
+    return axios.get("http://localhost:3001/api/notes")
+    .then(response =>{
+        const {data} = response
+        console.log(data)
+        return data
+    })
+}   
