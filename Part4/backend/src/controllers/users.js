a695123d466bc0c847a8781d56698e975bd24d0b
IgnaDev <arcortoon@gmail.com>
2023-12-24 20:22:04 -0300

    Ahora se crea la contraseña pero con hash

--- Part4/backend/src/controllers/users.js
+++ Part4/backend/src/controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/User')
 
@@ -5,10 +6,11 @@ usersRouter.post('/', async (req, res) => {
   const { body } = req
   const { username, name, password } = body
 
+  const passwordHash = await bcrypt.hash(password, 10)
   const user = new User({
     username,
     name,
-    passwordHash: password
+    passwordHash
   })
 
   const savedUser = await user.save()
