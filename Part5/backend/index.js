acefce58064715062da4580c95b822d98540d8cb
IgnaDev <arcortoon@gmail.com>
2023-12-30 19:02:19 -0300

    Ahora se crea un JWT por cada usuarios cuand se logea

--- Part5/backend/index.js
+++ Part5/backend/index.js
@@ -2,27 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const usersRouter = require('./src/controllers/users')
-// const logger = require('./loggerMiddleware')
 
-// Conexión con la base de datos
+const usersRouter = require('./src/controllers/users')
+const notesRouter = require('./src/controllers/notes')
+const loginRouter = require('./src/controllers/login')
+
 require('./mongo')
 
 const notFound = require('./src/middleware/notFound')
 const handleErrors = require('./src/middleware/handleErrors')
-const notesRouter = require('./src/controllers/notes')
-const loginRouter = require('./src/controllers/login')
+
+// Servidor
 
 app.use(cors())
 app.use(express.json())
-// Un middleware es una función que intercepta la petición que está atravesando tu API, permitiéndote realizar operaciones o aplicar lógica específica antes de que la solicitud alcance su destino final. Estas funciones juegan un papel crucial en la manipulación y el procesamiento de las solicitudes HTTP, ofreciendo un punto de intervención para personalizar el comportamiento de tu aplicación.
-
-// app.use(logger)
-
-// const app = http.createServer((request, response) =>{
-//     response.writeHead(200,{'Content-Type': 'application/json'})
-//     response.end(JSON.stringify(notes))
-// })
 
 app.get('/', (request, response) => {
   response.send('<h1>Hola mundo</h1>')
@@ -30,7 +23,7 @@ app.get('/', (request, response) => {
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
-app.use('/login', loginRouter)
+app.use('/api/login', loginRouter)
 
 app.use(notFound)
 app.use(handleErrors)
--- Part5/backend/src/controllers/login.js
+++ Part5/backend/src/controllers/login.js
@@ -1,21 +1,30 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
 const loginRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 
 loginRouter.post('/', async (req, res) => {
   const { body } = req
   const { username, password } = body
   const user = await User.findOne({ username })
   const passwordCorrect = user === null ? false : await bcrypt.compare(password, user.passwordHash)
-  if (!passwordCorrect) {
+  if (!(user && passwordCorrect)) {
     res.status(401).json({
       error: 'invalid user or password'
     })
   }
 
+  const userForToken = {
+    name: user._id,
+    username: user.username
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
   res.send({
     name: user.name,
-    username: user.username
+    username: user.username,
+    token
   })
 })
 
